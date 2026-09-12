fe6c0054ad88d47a41c53b08bb7771db832234ea
haruya-hayashi-lvgs <haruya.hayashi@leverages.jp>
2026-09-12 18:28:28 +0900

    asset: compose premium hero render

---
+++ lib/heroImage.ts
@@ -0,0 +1,6 @@
+import c0 from './heroChunk0';
+import c1 from './heroChunk1';
+import c2 from './heroChunk2';
+import c3 from './heroChunk3';
+
+export const HERO_IMAGE = `data:image/webp;base64,${c0}${c1}${c2}${c3}`;
